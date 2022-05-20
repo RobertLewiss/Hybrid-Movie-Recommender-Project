0861d978220b3c747b2cb9459002130767d6a638
RobertLewiss <100618730+RobertLewiss@users.noreply.github.com>
2022-05-20 17:44:26 +0100

    EDA & evaluation Notebook added, and Collab filtering methods
    We make a new Notebook for evaluation. We also add some more collaborative filtering methods (memory based)

--- Utils.py
+++ Utils.py
@@ -1,34 +1,77 @@
 import pandas as pd
-from nltk.stem.snowball import SnowballStemmer
-#from surprise import Reader, Dataset
 import numpy as np 
+
+from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
+import nltk
+
 import string
+
 import keras
 from keras.preprocessing.text import one_hot,Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
 from keras.layers import Dense , Flatten ,Embedding,Input
 from keras.models import Model
-import nltk
 
+import re
+
+from gensim.corpora import Dictionary
+from gensim.similarities import WordEmbeddingSimilarityIndex
+from gensim.similarities import SoftCosineSimilarity, SparseTermSimilarityMatrix
+from gensim.models import Word2Vec
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+# Importing modules
+import os
+
+# LDA Model
+import gensim
+from gensim.utils import simple_preprocess
+import gensim.corpora as corpora
+from pprint import pprint
+from gensim.models import CoherenceModel
+import spacy
+# Import the wordcloud library
+#from wordcloud import WordCloud
+# Visualize the topics
+import pyLDAvis.gensim_models
+import pickle 
+import pyLDAvis
+
+
+
+
+
+############################################
+# Generic Functions
+############################################
 
 def random_sample(arr: np.array, size: int = 10000) -> np.array:
+    """Samples from a Numpy array"""
     return arr[np.random.choice(len(arr), size=size, replace=False)]
 
 
-import nltk
-import re
-import string
-from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer # or LancasterStemmer, RegexpStemmer, SnowballStemmer
-
-default_stemmer = PorterStemmer()
-default_stopwords = stopwords.words('english') # or any other list of your choice
 def clean_text(text, toke = False):
+    """
+    Cleans a string of text
+    
+    Input: 
+    Text (String): Text to be cleaned
+    toke (Boolean): If True, function returns text in tokens.
+    
+    Returns:
+    text (String): Cleaned text
+    
+    """
+    default_stemmer = PorterStemmer()
+    default_stopwords = stopwords.words('english')
     
     
     def tokenize_text(text):
@@ -60,30 +103,105 @@ def clean_text(text, toke = False):
 
     return text
 
-def top_rec(title, sim, indices = pd.Series(df.index, index=df['title'])):
-    idx = indices[title]
-    sim_scores = list(enumerate(sim[idx]))
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:11]
-    movie_indices = [i[0] for i in sim_scores]
-    return df['title'].iloc[movie_indices]
+
+def top_rec(title_of_movie, similarity_matrix, indices, df):
+    """
+    Given a cosine siilarity matrix and item to id mapping, this function returns the top 10 items with the highest similarity.
+    
+    Input: 
+    title_of_movie (String): title of movie
+    similarity_matrix (2D array): similarity matrix
+    indices (DataFrame): Pandas DF, title of movies as index and values are ID
+    
+    Return:
+    top (Series): Pandas Series, Top movies with highest cosine score
+    
+    """
+
+    movie_indices = [i[0] for i in sorted(list(enumerate(similarity_matrix[indices[title_of_movie]])), key=lambda x: x[1], reverse=True)[1:11]]
+    
+    top  = df['title'].iloc[movie_indices]
+    return top
 
 
 
 
 
-from gensim.corpora import Dictionary
-from gensim.similarities import WordEmbeddingSimilarityIndex
-from gensim.similarities import SoftCosineSimilarity, SparseTermSimilarityMatrix
-from nltk import word_tokenize
-from nltk.corpus import stopwords
-from gensim.models import Word2Vec
+
+
+############################################
+# Content Based models
+############################################
+
+def TF_IDF(column, ngram = 2):
+    
+    tf = TfidfVectorizer(analyzer='word',ngram_range=(1, ngram),min_df=0, stop_words='english')
+    tfidf_matrix = tf.fit_transform(column)
+    return linear_kernel(tfidf_matrix, tfidf_matrix)
+    
+
+def Word2Vec_Hybrid(text_token, vector_size = 300, window = 7, epochs = 25):
+
+    corpus_sentences = text_token
+
+    model = Word2Vec(vector_size=vector_size, window=window, min_count=5, workers=11, alpha=0.025)
+
+    model.build_vocab(corpus_sentences)
+
+    model.train(corpus_sentences, total_examples=model.corpus_count, epochs=epochs)
+    
+    termsim_index = WordEmbeddingSimilarityIndex(model.wv) # similartiy between word embeddings
+
+
+    corpus_list_token = text_token.tolist() # list of tokenized sentences
+
+    dictionary = Dictionary(corpus_list_token) #word id and word
+
+    bow_corpus = [dictionary.doc2bow(document) for document in corpus_list_token] # word id and frequency for each doc
+
+    similarity_matrix = SparseTermSimilarityMatrix(termsim_index, dictionary)
+
+    
+    # a method that allows us to assess the similarity between two documents in a meaningful way, even when they have no words in common. It uses a measure of similarity between words
+    docsim_index = SoftCosineSimilarity(bow_corpus, similarity_matrix)
+
+    return docsim_index[bow_corpus]
+
+
+
+def Doc2Word_embed(text_clean, text_token, vector_size = 300, window = 7,epochs = 25):
+    corpus_sentences = text_token
+
+    model = Word2Vec(vector_size=vector_size, window=window, min_count=5, workers=11, alpha=0.025)
+
+    model.build_vocab(corpus_sentences)
+
+    model.train(corpus_sentences, total_examples=model.corpus_count, epochs=epochs)
+
+    word_em = model.wv.vectors
+
+    embeddings_dict = {}
+    for word in model.wv.index_to_key:
+        embeddings_dict[word] = model.wv[word]
+
+    def doc2vecF(doc):
+        vdoc = [embeddings_dict.get(x,0) for x in doc.lower().split(" ")]
+        doc2vec = np.sum(vdoc, axis = 0)
+        if np.sum(doc2vec == 0) ==1:
+            print("hi")
+            doc2vec = np.zeros(50, "float32")
+
+        return doc2vec
+
+    data = []
+    for i in text_clean:
+        data.append(doc2vecF(i))
+    embd = pd.DataFrame(data)
+
+    return cosine_similarity(embd, embd)
 
 
 
 
-
-
-
-
-
+def Rating2Vec(user_item_pivot_df):
+    return cosine_similarity(user_item_pivot_df, user_item_pivot_df)
