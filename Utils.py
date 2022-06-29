7d7a554d4c57db0e58bf5ec7fe67b59973e00239
RobertLewiss <100618730+RobertLewiss@users.noreply.github.com>
2022-06-29 14:06:53 +0100

    Finalising everything
    Cleaned notebooks and provided better comments

--- Utils.py
+++ Utils.py
@@ -45,7 +45,7 @@ import pyLDAvis.gensim_models
 import pickle 
 import pyLDAvis
 
-
+import matplotlib.pyplot as plt
 
 
 
@@ -327,3 +327,52 @@ def CB_CF_MEMORY_HYBRID_REC_test( top_matrix, user, df, n):
                                          '4th Max', '5th Max', '6th Max', '7th Max','8th Max', '9th Max','10th Max']].values[0]
 
     return df[['title','movieId']].iloc[indx][0:n]
+
+
+
+
+
+def utils_plot_keras_training(training):
+    metrics = [k for k in training.history.keys() if ("loss" not in k) and ("val" not in k)]
+    fig, ax = plt.subplots(nrows=1, ncols=2, sharey=True, figsize=(15,3))
+    
+    ## training
+    ax[0].set(title="Training")
+    ax11 = ax[0].twinx()
+    ax[0].plot(training.history['loss'], color='black')
+    ax[0].set_xlabel('Epochs')
+    ax[0].set_ylabel('Loss', color='black')
+    for metric in metrics:
+        ax11.plot(training.history[metric], label=metric)
+    ax11.set_ylabel("Score", color='steelblue')
+    ax11.legend()
+    
+    ## validation
+    ax[1].set(title="Validation")
+    ax22 = ax[1].twinx()
+    ax[1].plot(training.history['val_loss'], color='black')
+    ax[1].set_xlabel('Epochs')
+    ax[1].set_ylabel('Loss', color='black')
+    for metric in metrics:
+        ax22.plot(training.history['val_'+metric], label=metric)
+    ax22.set_ylabel("Score", color="steelblue")
+    plt.show()
+    
+    
+def NN_Test(test,n = 5):
+    tp = 0
+    
+    for user in range(test.user.nunique()):
+        top = n
+        y_test = test[test["user"]==user].sort_values("y", ascending=False)["product"].values[:top]
+
+        predicted = test[test["user"]==user].sort_values("yhat", ascending=False)["product"].values[:top]
+        
+        tp += len(list(set(y_test) & set(predicted)))
+    
+    return tp/test.user.nunique()    
+    
+    
+    
+    
+    
